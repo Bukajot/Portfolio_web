ea79905f84c4255e6163d01f88bbf1a0634bf59e
Jakub Kubacki <jakub.kubacki.jk@gmail.com>
2025-03-26 14:24:03 +0100

    Add some file

---
+++ script.js
@@ -0,0 +1,21 @@
+// !!!DO NAUCZENIA !!!
+
+// // function scrolltoelement(elementSelector, instance = 0) {
+// //     const element = document.querySelectorAll(elementSelector);
+// //     if(element.length > instance){
+// //         element[instance].scrollIntoView({behavior: "smooth"});
+// //     }
+// // }
+// // const link1 = document.getElementbyId("link1");
+// // const link2 = document.getElementbyId("link2");
+// // const link3 = document.getElementbyId("link3");
+
+// // link1.addEventListener("click", () => {
+// //     scrolltoelement(".header", null)
+// // });
+// // link2.addEventListener("click", () => {
+// //     scrolltoelement(".header", null)
+// // });
+// // link3.addEventListener("click", () => {
+// //     scrolltoelement(".column", null)
+// // });
